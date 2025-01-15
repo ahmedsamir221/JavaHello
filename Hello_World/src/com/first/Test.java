8ee2399d24d403ed3b9e438b58c51446c9b56c28
Ahmed Samir <asamir2211997@gmail.com>
2025-01-15 02:26:15 +0200

    init

---
+++ Hello_World/src/com/first/Test.java
@@ -0,0 +1,20 @@
+package com.first;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 1, 2, 3 };
+
+		int sum = 0;
+		int x = 55;
+
+		for (int num : numbers) {
+			sum += num;
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
---
+++ Hello_World/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PC
+ *
+ */
+module Hello_World {
+}
